bf3159938b29a19e85d5fcfa0173ea220bfe1bca
yeochinyi <yeochinyi@gmail.com>
2012-11-09 19:32:51 +0800

    clean up

--- src/main/webapp/console.groovy
+++ src/main/webapp/console.groovy
@@ -9,6 +9,26 @@ if (!session) {
 	session = request.getSession(true)
 }
 
+if(!application.externalFiles){
+	
+	def externalFiles = ''
+	
+	new File(context.getRealPath("/")).eachFileRecurse groovy.io.FileType.FILES, {
+		def relativePath =  {i -> i.path.substring( i.path.indexOf("codemirror")).replace('\\','/')}
+		switch(it.name){
+			case ~'^.*\\.js$':
+			externalFiles <<= '<script src="' + relativePath(it) + '"></script>\n'
+			break
+			
+			case ~'^.*\\.css$':
+			externalFiles <<= '<link rel="stylesheet" href="' +  relativePath(it) + '">\n'
+			break
+		}
+	}
+	
+	application.externalFiles = externalFiles
+}
+
 def baseDir = System.getProperty("user.home") + "/groovy_script/"
 
 new File(baseDir).mkdirs()
@@ -22,24 +42,35 @@ def file = params.file ?: "~"
 html.html{
 	head{
 		title("Groovy Web Console")
-		println """
+		
+		//println new File(context.getRealPath("/") + "/" + context.contextPath)
 
-<link rel="stylesheet" href="codemirror/codemirror.css">
-<link rel="stylesheet" href="codemirror/theme/blackboard.css">
-<link rel="stylesheet" href="codemirror/util/simple-hint.css">
-<link rel="stylesheet" href="codemirror/util/dialog.css">
+println """
+${application.externalFiles}
 
-<script src="codemirror/codemirror.js"></script>
-<script src="codemirror/groovy.js"></script>
-<script src="codemirror/util/dialog.js"></script>
-<script src="codemirror/util/search.js"></script>
-<script src="codemirror/util/searchcursor.js"></script>
-<script src="codemirror/util/simple-hint.js"></script>
-<script src="codemirror/util/foldcode.js"></script>
-<script src="codemirror/util/match-highlighter.js"></script>
-<script src="codemirror/util/closetag.js"></script>
-
-<style>.CodeMirror {border-top: 1px solid #500; border-bottom: 1px solid #500;}</style>
+<style type="text/css">
+.CodeMirror {border-top: 1px solid #500; border-bottom: 1px solid #500;}
+.CodeMirror-fullscreen {
+        display: block;
+        position: absolute;
+        top: 0; left: 0;
+        width: 100%;
+        z-index: 9999;
+       }
+.cm-tab {
+         background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAMCAYAAAAkuj5RAAAAAXNSR0IArs4c6QAAAGFJREFUSMft1LsRQFAQheHPowAKoACx3IgEKtaEHujDjORSgWTH/ZOdnZOcM/sgk/kFFWY0qV8foQwS4MKBCS3qR6ixBJvElOobYAtivseIE120FaowJPN75GMu8j/LfMwNjh4HUpwg4LUAAAAASUVORK5CYII=);
+         background-position: right;
+         background-repeat: no-repeat;       
+.CodeMirror-gutter {
+        width: 3em;
+        background: white;
+      }         
+.CodeMirror-scroll {
+  height: auto;
+  overflow-y: hidden;
+  overflow-x: auto;
+}
+</style>
 <script>
 </script>
 """
@@ -78,9 +109,9 @@ html.html{
 					session["file:" + prevfile] = code
 				}
 									
-				//Load from file if not in buffer	
+				//Load from file if not in buffer
 				if(!session["file:" + file]){
-					reload.call()			
+					reload.call()
 				}
 				else{
 					code = session["file:" + file]
@@ -121,32 +152,108 @@ html.html{
 		}
 		
 		h5 "Base dir->" + baseDir
+
+		println """Theme: <select onchange="selectTheme()" id=select>
+    <option selected>default</option>
+    <option>ambiance</option>
+    <option>blackboard</option>
+    <option>cobalt</option>
+    <option>eclipse</option>
+    <option>elegant</option>
+    <option>erlang-dark</option>
+    <option>lesser-dark</option>
+    <option>monokai</option>
+    <option>neat</option>
+    <option>night</option>
+    <option>rubyblue</option>
+    <option>vibrant-ink</option>
+    <option>xq-dark</option>
+</select>"""
 					
 		form(method:"post"){
 			h2 "GWC Enter code:"
 			textarea(id:"code", name:"code", rows:"40", cols:"80", code)
 			select(name:"file",onchange:"forms[0].action.value='changefile';forms[0].submit()"){
 				option("~")
-				new File(baseDir).eachFile { 
+				new File(baseDir).eachFile {
 					//show * if in buffer
 					def mod = session["file:" + it.name] ? "*" : ""
 					println "<option ${it.name==file ?'selected':''} value=${it.name} >${it.name}${mod}</option>"
 				}
-			}			
+			}
 			input(type:"hidden", name:"newfile")
 			input(type:"hidden", name:"action")
 			input(type:"button", value:"run", onclick:"forms[0].action.value='run';forms[0].submit()")
 			input(type:"button", value:"save", onclick:"forms[0].newfile.value = prompt ('filename ?','${file}');if(forms[0].newfile.value) forms[0].action.value='save';forms[0].submit()")
 			input(type:"button", value:"reload", onclick:"forms[0].action.value='reload';forms[0].submit()")
 			input(type:"button", value:"clear", onclick:"forms[0].action.value='clear';forms[0].submit()")
+			input(type:"button", value:"format", onclick:"autoFormatSelection()")
 		}
 		println """
  <script>
+ 
+     function isFullScreen(cm) {
+      return /\bCodeMirror-fullscreen\b/.test(cm.getWrapperElement().className);
+    }
+    function winHeight() {
+      return window.innerHeight || (document.documentElement || document.body).clientHeight;
+    }
+    function setFullScreen(cm, full) {
+      var wrap = cm.getWrapperElement(), scroll = cm.getScrollerElement();
+      if (full) {
+        wrap.className += " CodeMirror-fullscreen";
+        scroll.style.height = winHeight() + "px";
+        document.documentElement.style.overflow = "hidden";
+      } else {
+        wrap.className = wrap.className.replace(" CodeMirror-fullscreen", "");
+        scroll.style.height = "";
+        document.documentElement.style.overflow = "";
+      }
+      cm.refresh();
+    }
+    CodeMirror.connect(window, "resize", function() {
+      var showing = document.body.getElementsByClassName("CodeMirror-fullscreen")[0];
+      if (!showing) return;
+      showing.CodeMirror.getScrollerElement().style.height = winHeight() + "px";
+    });
+ 
       var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
         lineNumbers: true,
-        matchBrackets: true,
-        mode: "text/x-groovy"
-      });
+        matchBrackets: true,        
+        tabSize: 2,
+        indentUnit: 2,
+        mode: 'text/x-groovy',
+        extraKeys: {
+          'F11': function(cm) {setFullScreen(cm, !isFullScreen(cm));},
+          'Esc': function(cm) {if (isFullScreen(cm)) setFullScreen(cm, false);}
+        }
+      }
+      );
+   function getSelectedRange() {
+        return { from: editor.getCursor(true), to: editor.getCursor(false) };
+      }
+      
+      function autoFormatSelection() {
+        var range = getSelectedRange();
+        editor.autoFormatRange(range.from, range.to);
+      }
+      
+      function commentSelection(isComment) {
+        var range = getSelectedRange();
+        editor.commentRange(isComment, range.from, range.to);
+      }            
+      
+  var input = document.getElementById("select");
+  function selectTheme() {
+    var theme = input.options[input.selectedIndex].innerHTML;
+    editor.setOption("theme", theme);
+  }
+  var choice = document.location.search && document.location.search.slice(1);
+  if (choice) {
+    input.value = choice;
+    editor.setOption("theme", choice);
+  }      
+            
     </script>
 """
 		h2 "Output:"
@@ -163,4 +270,3 @@ html.html{
 
 	}
 }
-
